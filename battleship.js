2c32ac61753f89d00226602a1e49551eec299d7c
Vando dos reis <vandodosreis2001@gmail.com>
2021-03-21 19:33:05 -0300

    Criando as localizaçoes do navios

--- battleship.js
+++ battleship.js
@@ -1,12 +1,22 @@
+/* Visivel para o usuario */
 let view = {
   displayMessage(msg) {
     let messageArea = document.getElementById("messageArea");
     messageArea.innerHTML = msg;
   },
   displayHit(location) {
-
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
   },
   displayMiss(location) {
-
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
   }
 }
+
+/* Localização dos navios */
+let ships = [
+  { location: ["10", "20", "30"], hits: ["", "", ""] },
+  { location: ["32", "33", "34"], hits: ["", "", ""] },
+  { location: ["63", "64", "65"], hits: ["", "", ""] }
+];
