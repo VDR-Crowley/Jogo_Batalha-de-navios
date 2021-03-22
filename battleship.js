68136ffeb5a42116b60a73cdeb3c4aedecf92180
Vando dos reis <vandodosreis2001@gmail.com>
2021-03-22 12:06:23 -0300

    Controllers criado e funcionando

--- battleship.js
+++ battleship.js
@@ -15,8 +15,86 @@ let view = {
 }
 
 /* Localização dos navios */
-let ships = [
-  { location: ["10", "20", "30"], hits: ["", "", ""] },
-  { location: ["32", "33", "34"], hits: ["", "", ""] },
-  { location: ["63", "64", "65"], hits: ["", "", ""] }
-];
+
+let model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    { locations: ["10", "20", "30"], hits: ["", "", ""] },
+    { locations: ["32", "33", "34"], hits: ["", "", ""] },
+    { locations: ["63", "64", "65"], hits: ["", "", ""] }
+  ],
+
+  fire(guess) {
+    for(let i = 0; i < this.numShips; i++) {
+      let ship = this.ships[i];
+      let index = ship.locations.indexOf(guess);
+      if(index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+        if(this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayHit(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+
+  isSunk(ship) {
+    for(let i = 0;  i < this.shipLength; i++) {
+      if(ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+let controller = {
+  guesses: 0,
+
+  processGuess(guess) {
+    let location = parseGuess(guess);
+    if (location) {
+      this.guesses++;
+      let hit = model.fire(location);
+      if(hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all my battleship, in "+this.guess+" guesses");
+      }
+    }
+  }
+};
+
+function parseGuess(guess) {
+  let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+  if(guess === null || guess.length !== 2) {
+    alert("Ops, please enter a letter and a number on the board");
+  } else {
+    let firstChar = guess.charAt(0);
+    let row = alphabet.indexOf(firstChar);
+    let column = guess.charAt(1);
+
+    if(isNaN(row) || isNaN(column)) {
+      alert("Ops, that isn't on the board.");
+    } else if(row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+      alert("Ops, that's off the board");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
+
+controller.processGuess("A0");
+controller.processGuess("C5");
+controller.processGuess("B6");
+controller.processGuess("G4");
